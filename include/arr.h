8aeba3d42c89e8a080742b53c498e386384c38eb
Me1Am <jtkowalew@gmail.com>
2025-03-10 16:04:24 -0400

    Finished macro implementations

--- include/arr.h
+++ include/arr.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 
 
@@ -11,22 +12,69 @@
 #endif
 
 
+
+/**
+ *  Array modifiers
+ */
+/// Initialize the array
+#define arr_init(a) arr_init_cap(a, sizeof(*(a)), 4)
+/// Delete all elements in the array
+#define arr_clr(a) (get_array_header(a) = (array_header) { 0 })
+/// Free the array
+#define arr_free(a) (free(stbds_header(a)), (a) = NULL)
+
+/**
+ *  Header operations
+ */
 /// Returns a pointer to the array header
 #define get_array_header(a) ((array_header*)((uint8_t*)(a) - sizeof(array_header)))
+/// Returns the number of elements that the array has allocated space for
+#define arr_cap(a) (get_array_header(a)->capacity)
 /// Returns the length of the array
-#define array_length(a) ((a) ? get_array_header(a)->length : 0)
-/// Returns the total allocated space of the array
-#define array_capacity(a) ((a) ? get_array_header(a)->capacity : 0)
+#define arr_len(a) (get_array_header(a)->length)
+/// If the array has a length of 0
+#define arr_empty(a) (arr_len(a) == 0)
 
-
+/**
+ *  Length and capacity modifiers
+ */
 /// Grow the array for the given number of elements if needed
-#define array_maybe_grow(a, n)  ((array_length(a) + n > array_capacity(a)) ? array_grow(a, sizeof(*(a)) : 0)
-/// Push the given element to the back
-#define array_push(a, v) (array_maybe_grow(a, 1), (a)[get_array_header(a)->length++] = (v))
-
-// Aliases
+#define array_maybe_grow(a, n) ((arr_len(a) + n > arr_cap(a)) ? ((a) = array_grow(a, sizeof(*(a)), n)),0 : (get_array_header(a)->length += n))
+/// Sets the capacity of the array, returns a new pointer on success
 #define arr_setcap(a, c) (array_set_capacity((a), sizeof(*(a)), c))
-#define arr_setlen(a, l) (array_grow((a), sizeof(*(a)), c))
+#define arr_reserve_exact(a, c) arr_setcap(a, c)
+/// Increase the length of the array without initializing the new elements
+#define arr_setlen(a, l) (array_maybe_grow((a), sizeof(*(a)), l))
+
+/**
+ *  Element modifiers and accessors
+ */
+/// Safe accessor with bounds checking
+#define arr_at(a, i) ((i < arr_len(a)) ? a[i] : 0)
+/// Insert an element in the array at the given element
+#define arr_ins(a, i, e) ( \
+    array_maybe_grow(a, 1), \
+    memmove(&(a)[i + 1], &(a)[i], (arr_len(a) - (i) - 1) * sizeof(*(a))), \
+    (a)[i] = (e) \
+)
+/// Push the given element to the back, will reallocate if necessary
+#define arr_push(a, e) (array_maybe_grow(a, 1), (a)[arr_len(a)-1] = (e))
+/// Removes the last element and returns it
+#define arr_pop(a) ( \
+    get_array_header(a)->length--, \
+    (a)[arr_len(a)] \
+)
+/// Delete the element at the given index
+#define arr_del(a, i) arr_delr(a, i, 1)
+/// Delete n number of elements starting at index i
+#define arr_delr(a, i, n) ( \
+    memmove(&(a)[i], &(a)[(i)+(n)], sizeof(*(a)) * (arr_len(a) - (i) - (n))), \
+    get_array_header(a)->length -= (n) \
+)
+/// Delete the given element and swap it with the last
+#define arr_dels(a, i) (a[i] = a[arr_len(a) - 1], arr_pop(a))
+
+
 
 typedef struct {
     size_t capacity;
@@ -34,21 +82,13 @@ typedef struct {
 } array_header;
 
 
-
-/// Inititalize the array
-void* arr_init(void* arr, size_t element_size) NONNULL;
-
-/// Inititalize the array with the given length
-void* arr_init_length(void* arr, size_t element_size, size_t length) NONNULL;
-
-/// Push an element to the back of the element
-void arr_push(void* arr, void* element);
+/// Inititalize the array with the given capacity
+void* arr_init_cap(void* arr, size_t element_size, size_t capacity) NONNULL;
 
 /// Increase the array's length by the given amount
-/// Consider using the macro array_set_len
 void* array_grow(void* arr, size_t element_size, size_t add_length) NONNULL;
 
-/// Set the array's capacity
+/// Set the array's capacity, `arr` will be invalidated upon successful reallocation
 void* array_set_capacity(void* arr, size_t element_size, size_t capacity) NONNULL;
 
 #endif
--- src/arr.c
+++ src/arr.c
@@ -2,16 +2,11 @@
 
 #include "arr.h"
 
-size_t maxs(size_t a, size_t b) { return (a > b) ? a : b; }
 
-/// Calls arr_init_length with a length of 1
-void* arr_init(void* arr, size_t element_size) {
-    return arr_init_length(arr, element_size, 1);
-}
 
-/// Inititalize the array with the given length
-void* arr_init_length(void* arr, size_t element_size, size_t length) {
-    arr = malloc(sizeof(array_header) + (element_size * length));
+/// Inititalize the array with the given capacity
+void* arr_init_cap(void* arr, size_t element_size, size_t capacity) {
+    arr = malloc(sizeof(array_header) + (element_size * capacity));
 
     if(arr == NULL)
         return NULL;
@@ -20,40 +15,48 @@ void* arr_init_length(void* arr, size_t element_size, size_t length) {
 
     *(array_header*)arr = (array_header) {
         .length = 0,
-        .capacity = length,
+        .capacity = capacity,
     };
 
     return arr_ptr;
 }
 
+/// Realloc exactly enough memory for `capacity`
+/// The array pointer will be invalidated on success, just like realloc
 void* array_set_capacity(void* arr, size_t element_size, size_t capacity) {
-    if(capacity < array_capacity(arr))
+    if(capacity < arr_cap(arr))
         return arr;
 
     // Attempt to grow exponentially, else set absolute capacity
-    const size_t new_capacity = maxs(array_capacity(arr) * 2, capacity);
-    void* temp = realloc(get_array_header(arr), sizeof(array_header) + (element_size * new_capacity));
+    void* temp = realloc(get_array_header(arr), sizeof(array_header) + (element_size * capacity));
     temp = ((uint8_t*)temp) + sizeof(array_header);
 
     if(temp == NULL)
         return NULL;
 
     // Success, update capacity
-    get_array_header(temp)->capacity = new_capacity;
+    get_array_header(temp)->capacity = capacity;
 
-    return arr;
+    return temp;
 }
 
-/// Calls array_set_capacity if add_length + array_length is greater than the capacity
+/// Calls array_set_capacity if `add_length` + array_length is greater than the capacity
+///  Does nothing if minimum length is less than the capacity
+///  Uninitialized slots will remain uninitialized
 void* array_grow(void* arr, size_t element_size, size_t add_length) {
-    const size_t minimum_length = array_length(arr) + add_length; // Desired length
+    size_t minimum_length = arr_len(arr) + add_length; // Desired length
 
     // Don't need to realloc
-    if(minimum_length <= array_capacity(arr))
+    if(minimum_length <= arr_cap(arr)) {
+        get_array_header(arr)->length += add_length;
         return arr;
+    }
 
-    // Success, update length
-    void* temp array_set_capacity(arr, element_size, minimum_length);
+    // Will attempt to use exponential growth
+    if(minimum_length < arr_cap(arr) * 2)
+        minimum_length = arr_cap(arr) * 2;
+
+    void* temp = array_set_capacity(arr, element_size, minimum_length);
     get_array_header(temp)->length += add_length;
 
     return temp;
